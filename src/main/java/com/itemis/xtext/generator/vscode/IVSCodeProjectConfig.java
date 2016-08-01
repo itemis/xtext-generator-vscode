cbfcebd673d69dbac7a15451c0a429892069c363
Karsten Thoms <karsten.thoms@itemis.de>
2016-08-02 01:29:36 +0200

    Removed special project config again - not convenient to use

--- src/main/java/com/itemis/xtext/generator/vscode/IVSCodeProjectConfig.java
+++
@@ -1,8 +0,0 @@
-package com.itemis.xtext.generator.vscode;
-
-import org.eclipse.xtext.xtext.generator.model.project.ISubProjectConfig;
-import org.eclipse.xtext.xtext.generator.model.project.IXtextProjectConfig;
-
-public interface IVSCodeProjectConfig extends IXtextProjectConfig {
-	ISubProjectConfig getVsExtension();
-}
